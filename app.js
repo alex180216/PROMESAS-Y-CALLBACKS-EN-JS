fddeb71a70ee9d4ed1eb12bb350c9b4437189cac
Ale Mendoza <54868313+alex180216@users.noreply.github.com>
2020-06-25 22:51:57 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+/*USO DE LAS PROMISE y CALLBACKS */
+
+/*PROMESAS: Se usan en codigo asincrono, y generalmente cuando se usan API'S*/
+/*CALLBACKS: Tambien se usan en codigo asincrono, y son funciones dentro de otras funciones*/
+//EJEMPLOS
+
+/*Las promises deben instanciarse de la siguiente manera:
+
+    const nombrePromesa = new Promise(parametro1, parametro2);
+
+    estos parametros, son: 
+                        resolve = cuando la promesa se cumple
+                        reject  = cuando no se cumple.
+
+Cuando una promesa se cumple, debe ser llamada con .then
+si no se cumple, debe ser tomado el error con .catch
+
+si no son llamadas o tomadas, NO SE EJECUTAN
+*/
+
+//ejemplo
+const aplicarDescuento = new Promise(function(resolve, reject){
+    const descuento = true; //cambiar a false o true, si se cumple o no
+    if(descuento){
+        resolve('Se aplicó el descuento');
+    }else{
+        reject('No se pudo aplicar el descuento');
+    }
+});
+
+//aHORA LAS LLAMAMOS
+
+aplicarDescuento
+.then(function(resultado){
+    console.log(resultado);
+})
+.catch(function(error){
+    console.log(error);
+});
